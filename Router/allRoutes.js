c0e99c5decbf14664414ba351ee608ddcf2f8925
nedirjasmine302-bit <nedirjasmine302@gmail.com>
2026-09-13 12:37:55 +0200

    Page management complete (html+css+js)

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -8,6 +8,7 @@ export const allRoutes = [
   new Route("/sign-in", "Connexion", "/pages/sign-in.html", "/assets/js/src/sign-in.js"),
   new Route("/favorites", "Mes favoris", "/pages/favorites.html", "/assets/js/src/favorites.js"),
   new Route("/forgot-password", "Mot de passe oublié", "/pages/forgot-password.html", "/assets/js/src/forgot-password.js"),
-  new Route("/reset-password", "Réinitialisation", "/pages/reset-password.html", "/assets/js/src/reset-password.js")];
+  new Route("/reset-password", "Réinitialisation", "/pages/reset-password.html", "/assets/js/src/reset-password.js"),
+  new Route("/management", "Gestion", "/pages/management.html", "/assets/js/src/management.js")];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Escale";
